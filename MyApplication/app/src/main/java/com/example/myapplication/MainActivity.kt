9f1e05e9310502e51b12cb3e3cc7452a04e7b0c2
hs-1971366-yoonsungSeo <115918346+hs-1971366-yoonsungSeo@users.noreply.github.com>
2024-05-04 10:53:57 +0900

    Update MainActivity.kt
    socket connect

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -19,6 +19,9 @@ import com.google.mlkit.vision.text.Text
 import com.google.mlkit.vision.text.TextRecognition
 import com.google.mlkit.vision.text.korean.KoreanTextRecognizerOptions
 import com.yalantis.ucrop.UCrop
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
 import okhttp3.Interceptor
 import okhttp3.MediaType
 import okhttp3.OkHttpClient
@@ -29,13 +32,23 @@ import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
+import java.io.BufferedReader
+import java.io.BufferedWriter
 import java.io.ByteArrayOutputStream
 import java.io.File
 import java.io.IOException
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.net.InetAddress
+import java.net.Socket
+import java.security.SecureRandom
+import java.security.cert.X509Certificate
 import java.text.SimpleDateFormat
 import java.util.Date
-
+import javax.net.ssl.*
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -46,7 +59,10 @@ class MainActivity : AppCompatActivity() {
     private val CAPTURE_IMAGE_REQUEST = 2
     private val CROP_IMAGE_REQUEST = 3
 
-    //private val apiClient = ApiClient.getApiClient()
+    private val retrofit = Retrofit.Builder()
+        .baseUrl("https://192.168.123.114:443/")
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -55,14 +71,28 @@ class MainActivity : AppCompatActivity() {
         imageView = findViewById(R.id.imageView)
         inputText = findViewById(R.id.inputText)
 
+        //sendDataToServer()
+
         val summarizeButton = findViewById<Button>(R.id.summarizeButton)
         val selectImageButton = findViewById<Button>(R.id.selectImageButton)
         val captureImageButton = findViewById<Button>(R.id.captureImageButton)
 
         summarizeButton.setOnClickListener {
+//            ignoreSSLCertificate { success ->
+//                if (success) {
+//                    val input = inputText.text.toString()
+//                    val summarizedText = summarizeText(input)
+//                    inputText.setText(summarizedText)
+//                    connect()
+//                } else {
+//                    // SSL 인증서 검증에 실패한 경우에 대한 처리
+//                    // 예를 들어 사용자에게 알림을 표시하거나 적절한 조치를 취할 수 있습니다.
+//                    println("노우 안도ㅓㅐㅆ더")
+//                }
+//            }
+
             val input = inputText.text.toString()
-            val summarizedText = summarizeText(input)
-            inputText.setText(summarizedText)
+            sendDataToServer(input)
         }
 
         selectImageButton.setOnClickListener {
@@ -76,27 +106,52 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-//        super.onActivityResult(requestCode, resultCode, data)
-//
-//        if (resultCode == Activity.RESULT_OK) {
-//            when (requestCode) {
-//                SELECT_IMAGE_REQUEST -> {
-//                    val selectedImageUri = data?.data
-//                    imageView.setImageURI(selectedImageUri)
-//                    selectedImageUri?.let {
-//                        val bitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, selectedImageUri)
-//                        //extractTextFromImage(bitmap)
-//                    }
-//                }
-//                CAPTURE_IMAGE_REQUEST -> {
-//                    val imageBitmap = data?.extras?.get("data") as Bitmap
-//                    imageView.setImageBitmap(imageBitmap)
-//                    //extractTextFromImage(imageBitmap)
-//                }
-//            }
-//        }
-//    }
+    private fun sendDataToServer(data: String? = null) {
+
+        val port = 5000 // HTTP 포트 80
+        CoroutineScope(Dispatchers.IO).launch {
+            val serverAddress = InetAddress.getByName("192.168.123.110")
+            val socket = Socket(serverAddress, port)
+            val outputStream = socket.getOutputStream()
+            val writer = BufferedWriter(OutputStreamWriter(outputStream))
+
+            val path = "/api/summary" // POST 요청을 받는 서버의 경로
+            val postData = data ?: "default data" // POST 요청 본문 데이터
+
+            // POST 요청 메시지 작성
+            val requestMessage = """
+        POST $path HTTP/1.1
+        Host: ${serverAddress.hostName}
+        Content-Type: application/x-www-form-urlencoded
+        Content-Length: ${postData.length}
+        
+        $postData
+    """.trimIndent()
+
+            // POST 요청 메시지를 서버로 전송
+            writer.write(requestMessage)
+            writer.flush()
+
+            val inputStream = socket.getInputStream()
+            val reader = BufferedReader(InputStreamReader(inputStream))
+            val response = StringBuilder()
+
+            // 서버로부터의 응답을 읽어옴
+            var line: String?
+            while (reader.readLine().also { line = it } != null) {
+                response.append(line).append('\n')
+            }
+
+
+            // UI 업데이트
+            withContext(Dispatchers.Main) {
+                inputText.setText(response.toString())
+            }
+
+            // 소켓 닫기
+            socket.close()
+        }
+    }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
@@ -128,6 +183,58 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    fun ignoreSSLCertificate(callback: (Boolean) -> Unit) {
+        println("나 호출됐어")
+        try {
+            val trustAllCerts: Array<TrustManager> = arrayOf(object : X509TrustManager {
+                override fun checkClientTrusted(chain: Array<X509Certificate?>?, authType: String?) {}
+                override fun checkServerTrusted(chain: Array<X509Certificate?>?, authType: String?) {}
+                override fun getAcceptedIssuers(): Array<X509Certificate> {
+                    println("나 해결됐어")
+                    return arrayOf()
+                }
+            })
+
+            val sslContext = SSLContext.getInstance("TLS")
+            sslContext.init(null, trustAllCerts, java.security.SecureRandom())
+
+            HttpsURLConnection.setDefaultSSLSocketFactory(sslContext.socketFactory)
+            HttpsURLConnection.setDefaultHostnameVerifier { _, _ -> true }
+
+            callback(true)
+        } catch (e: Exception) {
+            Log.e("SSS", "ignoreSSLCertificate error: ${e.message}", e)
+            callback(false)
+        }
+    }
+
+    fun getUnsafeOkHttpClient(): OkHttpClient.Builder {
+        val trustAllCerts = arrayOf<TrustManager>(object : X509TrustManager {
+            override fun checkClientTrusted(chain: Array<out X509Certificate>?, authType: String?) {
+
+            }
+
+            override fun checkServerTrusted(chain: Array<out X509Certificate>?, authType: String?) {
+
+            }
+
+            override fun getAcceptedIssuers(): Array<X509Certificate> {
+                return arrayOf()
+            }
+        })
+
+        val sslContext = SSLContext.getInstance("SSL")
+        sslContext.init(null, trustAllCerts, SecureRandom())
+
+        val sslSocketFactory = sslContext.socketFactory
+
+        val builder = OkHttpClient.Builder()
+        builder.sslSocketFactory(sslSocketFactory, trustAllCerts[0] as X509TrustManager)
+        builder.hostnameVerifier { hostname, session -> true }
+
+        return builder
+    }
+
     private fun extractTextAndUploadImage(bitmap: Bitmap) {
         // Crop된 이미지를 OCR을 수행하여 텍스트를 추출합니다.
         val image = InputImage.fromBitmap(bitmap, 0)
@@ -148,7 +255,7 @@ class MainActivity : AppCompatActivity() {
     private fun startUCrop(sourceUri: Uri) {
         val destinationUri = Uri.fromFile(createImageFile())
         UCrop.of(sourceUri, destinationUri)
-            .withAspectRatio(1F, 1F)
+            //.withAspectRatio(16F, 9F)
             .start(this, CROP_IMAGE_REQUEST)
     }
 
@@ -182,91 +289,55 @@ class MainActivity : AppCompatActivity() {
         return image
     }
 
-    data class ImageResponse(
-        @SerializedName("success") val success: Boolean,
-        @SerializedName("message") val message: String?,
-        @SerializedName("imageUrl") val imageUrl: String?
+
+    data class MyRequestData(
+        val key1: String
+        //val key2: Int
     )
 
-    data class Image(
-        val format: String,
-        val name: String,
-        val data: ByteArray?, // 이미지 데이터
-        val url: String
-    )
 
     private fun summarizeText(text: String): String {
         // 텍스트 요약 로직을 추가
         return "요약된 텍스트"
     }
 
-    //api에 이미지 전송 후 ocr 적용
-//    private fun uploadImageToServer(bitmap: Bitmap) {
-//        println("api접속?")
-//
-//        val imageView = ImageView(this)
-//        imageView.setImageBitmap(bitmap)
-//        //println("Bitmap 정보: Width=${bitmap.width}, Height=${bitmap.height}")
-//        val outputStream = ByteArrayOutputStream()
-//        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
-//        val byteArray = outputStream.toByteArray()
-//        val requestBody = RequestBody.create(MediaType.parse("image/jpeg"), byteArray)
-//        println(requestBody)
-//
-//        val service = apiClient.create(ImageUploadAPI::class.java)
-//        val call = service.uploadImage(requestBody)
-//
-//        call.enqueue(object : Callback<ImageResponse> {
-//            override fun onResponse(call: Call<ImageResponse>, response: Response<ImageResponse>) {
-//                if (response.isSuccessful) {
-//                    println("얏호")
-//                    val imageResponse = response.body()
-//                    // 서버에서 반환한 이미지 URL 또는 다른 필요한 정보를 사용하여 OCR API를 호출하고 결과를 처리
-//                } else {
-//                    // 서버로부터 응답이 실패한 경우 처리
-//                    println("오노우")
-//                    Log.e("API Error", "Failed to upload image: ${response.code()}")
-//                }
-//            }
-//
-//            override fun onFailure(call: Call<ImageResponse>, t: Throwable) {
-//                // 통신 실패 시 처리
-//                println("이런 미띤")
-//                Log.e("API Error", "Failed to upload image", t)
-//            }
-//        })
-//    }
+    interface MyApiService {
+        //@GET("/api/summary") // 실제 엔드포인트 URL을 여기에 작성
+        @POST("api/summary")
+        fun postDataToServer(@Body requestData: MyRequestData): Call<Void> // 반환 유형은 서버 응답을 나타냅니다. 이 경우에는 응답이 없음을 의미하는 Void 사용
+    }
+
+    private fun connect(){
+        // Retrofit 서비스 인스턴스 생성
+        println("뭐냐이거?")
+        val myApiService = retrofit.create(MyApiService::class.java)
+
+        // 요청 데이터 생성
+        val requestData = MyRequestData("value1")
+
+        val call = myApiService.postDataToServer(requestData)
+
+        call.enqueue(object : Callback<Void> {
+            override fun onResponse(call: Call<Void>, response: Response<Void>) {
+                if (response.isSuccessful) {
+                    println("연결 성공")
+                    // 성공적으로 요청을 보냈습니다.
+                    // 서버 응답을 처리할 수도 있습니다.
+                } else {
+                    println("연결 실패")
+                    // 서버가 실패 응답을 반환했습니다.
+                    // 실패 처리 코드를 여기에 작성합니다.
+                }
+            }
+
+            override fun onFailure(call: Call<Void>, t: Throwable) {
+                // 통신 실패 처리 코드를 여기에 작성합니다.
+                println("실패가 됐다고잉?")
+                Log.e("YMC", "stringToJson2: ${t.message}", t)
+
+            }
+        })
+    }
 }
 
-//postman 주소 연결
-//object ApiClient {
-//    private const val BASE_URL = "https://l24fiwe6s2.apigw.ntruss.com/custom/v1/29599/ec9ba461bdc586ea062462533b591567bb811be98192d90bd43dc52b083ae2b7/general/"
-//    fun getApiClient(): Retrofit {
-//        return Retrofit.Builder()
-//            .baseUrl(BASE_URL)
-//            .client(provideOkHttpClient(AppInterceptor()))
-//            .addConverterFactory(GsonConverterFactory.create())
-//            .build()
-//    }
-//
-//    private fun provideOkHttpClient(interceptor: AppInterceptor): OkHttpClient
-//            = OkHttpClient.Builder().run {
-//        addInterceptor(interceptor)
-//        build()
-//    }
-//
-//    class AppInterceptor : Interceptor {
-//        @Throws(IOException::class)
-//        override fun intercept(chain: Interceptor.Chain) : okhttp3.Response = with(chain) {
-//            val newRequest = request().newBuilder()
-//                .addHeader("X-OCR-SECRET", "clVFQ1Z3d0pBcEJFVWpNcXlxTGVIUm90ekF0aFBVcnQ=")
-//                .build()
-//            proceed(newRequest)
-//        }
-//    }
-//}
-//
-//interface ImageUploadAPI {
-//    @POST("/api/upload_image")
-//    fun uploadImage(@Body image: RequestBody): Call<MainActivity.ImageResponse>
-//}
+
